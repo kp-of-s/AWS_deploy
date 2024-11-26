6fcc6c27736767064069f87be08d5d89d6770f9b
kp-of-s <gimbhs@gmil.com>
2024-11-26 10:05:21 +0900

    Initial Commit

---
+++ src/main/java/com/lec/spring/HomeController.java
@@ -0,0 +1,24 @@
+package com.lec.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+
+    @GetMapping("/aws/v1")
+    public String hello(@RequestParam(defaultValue = "1") Integer num) {
+
+        if(num == 1)
+            log.info("1");
+        else if(num == -1)
+            log.info("-1");
+        else if(num == 0)
+            log.info("0");
+
+        return "<h1>hell</h1>";
+    }
+}
